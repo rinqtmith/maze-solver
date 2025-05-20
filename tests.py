9b26ddf23c676256c7752dfed14aed39ec1574cc
fatih <rinqtf@gmail.com>
2025-05-20 21:15:14 +0300

    breaking some walls

--- tests.py
+++ tests.py
@@ -34,6 +34,19 @@ class Tests(unittest.TestCase):
         for col in m._Maze__cells:  # type: ignore[attr-defined]
             self.assertEqual(len(col), num_rows)
 
+    def test_maze_break_entrance_and_exit(self):
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertEqual(
+            m1._Maze__cells[0][0].has_top_wall,  # type: ignore[attr-defined]
+            False,
+        )
+        self.assertEqual(
+            m1._Maze__cells[num_cols - 1][num_rows - 1].has_bottom_wall,  # type: ignore[attr-defined]
+            False,
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
